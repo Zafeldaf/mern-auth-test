c035bcfb17ac5b503b22cc59d5ead32f9fc2d2d5
Zohaib Arsalan <zohaibhstart@gmail.com>
2023-07-25 13:00:21 +0500

    Error middleware name changed

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,10 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import cookieParser from "cookie-parser";
-import { notFound, errorHandler } from "./middleware/errorMiddleWare.js";
 import connectDB from "./config/db.js";
 const port = process.env.PORT || 6000;
 import userRoutes from "./routes/userRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 connectDB();
 
